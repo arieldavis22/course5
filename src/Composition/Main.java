dc3a6c0d68c984338d4e6d0b0250131748263347
arieldavis22 <amanichai100@gmail.com>
2022-08-04 14:33:59 -0400

    composition

--- src/Composition/Main.java
+++ src/Composition/Main.java
@@ -1,2 +1,15 @@
-package Composition;public class Main {
+package Composition;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+
+        Monitor theMonitor = new Monitor("27inch Beast", "acer", 27, new Resolution(2540, 1140));
+
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+    }
 }
